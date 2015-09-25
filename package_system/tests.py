4d70fb204fecb2ae0133351e90f45394cd3c7bce
Joakim Saario <s@xyr.se>
2015-09-25 20:42:49 +0200

    Testing that dashes aren't present i python_friendly

--- package_system/tests.py
+++ package_system/tests.py
@@ -1,6 +1,6 @@
 import unittest
 import string
-from hypothesis import given, assume
+from hypothesis import given, assume, example
 from hypothesis.strategies import text
 from .python import friendly
 
@@ -19,3 +19,11 @@ class FriendlyTestCase(unittest.TestCase):
     @given(text())
     def test_friendly_is_ascii(self, name):
         self.assertTrue(all(is_ascii(x) for x in friendly(name)))
+
+    @given(text(alphabet=string.printable))  # TODO: Include some unicode
+    @example('arst-star')
+    def test_friendly_python_not_contains_dashes(self, name):
+        _, python_friendly = friendly(name)
+        assume(python_friendly != '')
+
+        self.assertNotIn('-', python_friendly)
