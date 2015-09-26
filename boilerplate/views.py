a9882f1524e0ecf29c5618f6c3f05585cfa8059d
Joakim Saario <s@xyr.se>
2015-09-26 18:59:11 +0200

    Added _simple_view_factory to boilerplate app

--- boilerplate/views.py
+++ boilerplate/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+def _simple_view_factory(*arg_names, template=None):
+    if not template:
+        raise TypeError('No template given')
+
+    def view(request, *args):
+        return render(
+            request, template,
+            {arg_name: args[pos]
+             for pos, arg_name in enumerate(arg_names)}
+        )
+
+    return view
+
+
+index = _simple_view_factory(template='index.html')
